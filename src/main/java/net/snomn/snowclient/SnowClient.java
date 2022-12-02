8c6f0ae34b5a4a9a782d1ea5b175cfb078985732
Snomn <simon.alexander.pettersen@gmail.com>
2022-12-02 22:08:05 +0100

    Initial commit

---
+++ src/main/java/net/snomn/snowclient/SnowClient.java
@@ -0,0 +1,21 @@
+package net.snomn.snowclient;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SnowClient implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger("snowclient");
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
