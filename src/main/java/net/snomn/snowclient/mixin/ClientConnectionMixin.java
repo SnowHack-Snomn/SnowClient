713348cfd726d9b4279d960f8caa608fdf34f3ad
Snomn <simon.alexander.pettersen@gmail.com>
2022-12-02 22:54:59 +0100

    Packet listener

---
+++ src/main/java/net/snomn/snowclient/mixin/ClientConnectionMixin.java
@@ -0,0 +1,18 @@
+package net.snomn.snowclient.mixin;
+
+import net.minecraft.network.ClientConnection;
+import net.minecraft.network.Packet;
+import net.minecraft.network.listener.PacketListener;
+import net.snomn.snowclient.SnowClient;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientConnection.class)
+public class ClientConnectionMixin {
+    @Inject(method = "handlePacket", at = @At("HEAD"), cancellable = true)
+    private static <T extends PacketListener> void onHandlePacket(Packet<T> packet, PacketListener listener, CallbackInfo info) {
+        SnowClient.LOGGER.info(packet.getClass().getName());
+    }
+}
